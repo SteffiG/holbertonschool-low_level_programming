9810469589d805a59cf8fba23a7cc3e57ec3754b
SteffiG <1644@holbertonschool.com>
2020-02-05 21:50:04 +0000

    Add files

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,8 @@
 #include <stdio.h>
 
-/**
- * main - Entry point
- *
- * Return: 0
- */
+/* main - Entry point, return: 0 */
 
-int main(void)
-{
-  puts("\"Programming is like building a multilingual puzzle\n");
+int main(void) {
+  puts("Programming is like building a multilingual puzzle"\n);
   return (0);
 }
