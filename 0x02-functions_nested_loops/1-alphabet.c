5c6f1e359e5791948f276db1df1994f67a4ec26c
SteffiG <1644@holbertonschool.com>
2020-02-11 15:36:40 +0000

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,11 @@
 #include "holberton.h"
 
 /**
- * main - Function main, check the code
- * print_alphabet - function that prints the alphabet
+ * Description: print_alphabet function that prints the alphabet
  * _putchar
  * Return: 0
  */
-void print_alphabet(void);
-int main(void)
+void print_alphabet(void)
 {
 char c;
 
@@ -16,5 +14,4 @@ for (c = 'a'; c <= 'z'; c++)
 _putchar(c);
 }
 _putchar('\n');
-return (0);
 }
