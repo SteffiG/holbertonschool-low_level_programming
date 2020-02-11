a980e4c989d723fa56a36e08526445b94f7c1ce3
SteffiG <1644@holbertonschool.com>
2020-02-11 17:03:20 +0000

    alphabet x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,27 +2,22 @@
 
 /**
  * print_alphabet_x10 - Function that prints 10 times the alphabet
- * _putchar 
+ * _putchar
  * Return: 0
  */
 
 void print_alphabet_x10(void)
 {
-
 int d;
 char c;
-
 d = 0;
 while (d <= 10)
 {
-
 for (c = 'a'; c <= 'z'; c++)
-
 {
 _putchar(c);
 }
-
- _putchar('\n');
-d++
+_putchar('\n');
+d++;
 }
 }
