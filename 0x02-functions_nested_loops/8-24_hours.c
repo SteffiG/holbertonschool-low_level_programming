f315369315cb89836f530ea256c6d2112da88c69
SteffiG <1644@holbertonschool.com>
2020-02-12 02:59:30 +0000

    hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer - Function that prints the hours and minutes for one day.
+ * _putchar
+ * Return: 0
+ */
+
+void jack_bauer(void)
+{
+int h, m;
+for (h = 0; h >= 23; h++)
+{
+for (m = 0; m >= 59; m++)
+{
+_putchar(h / 10 + '0');
+_putchar(h % 10 + '0');
+_putchar(':');
+_putchar(m / 10 + '0');
+_putchar(m % 10 + '0');
+_putchar('\n');
+}
+}
+}
