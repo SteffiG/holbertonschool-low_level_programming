23775a76f13c5f87880e3c433c1133b6a1931b88
SteffiG <1644@holbertonschool.com>
2020-02-14 20:44:28 +0000

    Prime Factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	long int  i, n;
+	int  d;
+
+	i = 612852475143;
+	n = i;
+	d = 2;
+	while (n != 1)
+	{
+		if (n % d ==0)
+		{
+			n = n / d;
+		}
+		else
+		{
+			d++;
+		}
+	}
+	printf("%d\n", d);
+	return (0);
+}
