e44e3084b67685fe96c42b7b15eca412c03b548a
SteffiG <1644@holbertonschool.com>
2020-02-20 21:15:53 +0000

    Copy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,28 +10,15 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	/**
 	int i;
 
-	for (i = 0; dest[i] != '\0'; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
-	return (0);
-	**/
-	int i;
-
-	for (i = 0; src[i] != '\0'; i++)
+	for ( ; i < n; i++)
 	{
-		/*	dest[i] = src[i];*/
-	}
-
-	for (i = 0; i < n; i++)
-	{
-		dest[i] = src[i];
-		if (dest[i] == '\0')
-			i = n;
-		/*dest[i] = '\0';*/
+		dest[i] = '\0';
 	}
 
 	return (dest);
